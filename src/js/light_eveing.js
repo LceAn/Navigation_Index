c5118ad58932a77c874128a6cbed0ca3b77211b0
LceAn <lcean@users.noreply.github.com>
2025-05-14 17:29:16 +0800

    update THE 250514 1729 of LceAn
      - 3.修复夜间模式，加入联动天空。
      - 4.修订“天空切换"功能
      - 5.修订"更多"界面的错位问题

--- src/js/light_eveing.js
+++ src/js/light_eveing.js
@@ -1,12 +1,20 @@
-<!--夜间模式按钮js -->
+// 夜间模式按钮js
 function toggleNightMode() {
+    // 1. 触发天空和太阳/月亮的动画切换
+    $("body").toggleClass("mk-dark-mode");
+    
+    // 2. 旋转太阳/月亮
+    var angle = $('.mk-dark-mode-planet').data('angle') + 360 || 360;
+    $('.mk-dark-mode-planet').css({'transform': 'rotate(' + angle + 'deg)' });
+    $('.mk-dark-mode-planet').data('angle', angle);
+    
+    // 3. 应用滤镜效果
     const docStyle = document.documentElement.style;
     if (!window.modeIndex) {
         window.modeIndex = 0;
     }
     const styleList = [
         '',
-        // 'invert(85%) hue-rotate(180deg)',
         'invert(100%) hue-rotate(180deg)',
     ];
     window.modeIndex = window.modeIndex >= styleList.length - 1 ? 0 : window.modeIndex + 1;
